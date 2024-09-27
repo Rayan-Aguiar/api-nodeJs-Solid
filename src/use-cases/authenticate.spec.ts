753a12e85f480b003b98460445866e2b5c520c56
rayan-aguiar <rayansiqueira@gmail.com>
2024-09-27 16:28:45 -0300

    fim aula F084 - Testes e Controller de autenticação

---
+++ src/use-cases/authenticate.spec.ts
@@ -0,0 +1,55 @@
+import { describe } from "node:test";
+import { it, expect } from "vitest";
+import { InMemoryUsersRepository } from "@/repositories/in-memory/in-memory-users-repository";
+import { AuthenticateUseCase } from "./authenticate";
+import { hash } from "bcryptjs";
+import { InvalidCredentialsError } from "./erros/invalid-credentials-error";
+
+describe("Authenticate Use Case", () => {
+  it("should be able to authenticate", async () => {
+    const usersRepository = new InMemoryUsersRepository();
+    const sut = new AuthenticateUseCase(usersRepository);
+
+    await usersRepository.create({
+      email: "john.doe@example.com",
+      name: "John Doe",
+      password_hash: await hash("123456", 6),
+    });
+
+    const { user } = await sut.execute({
+      email: "john.doe@example.com",
+      password: "123456",
+    });
+    expect(user.id).toEqual(expect.any(String));
+  });
+
+  it("should not be able to authenticate with wrong email", async () => {
+    const usersRepository = new InMemoryUsersRepository();
+    const sut = new AuthenticateUseCase(usersRepository);
+
+    expect(() => 
+      sut.execute({
+        email: "john.doe@example.com",
+        password: "123456",
+      }),
+    ).rejects.toBeInstanceOf(InvalidCredentialsError);
+  });
+
+  it("should not be able to authenticate with wrong password", async () => {
+    const usersRepository = new InMemoryUsersRepository();
+    const sut = new AuthenticateUseCase(usersRepository);
+
+    await usersRepository.create({
+        email: "john.doe@example.com",
+        name: "John Doe",
+        password_hash: await hash("123456", 6),
+      });
+
+    expect(() => 
+      sut.execute({
+        email: "john.doe@example.com",
+        password: "1234561",
+      }),
+    ).rejects.toBeInstanceOf(InvalidCredentialsError);
+  });
+});
--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -1,39 +1,41 @@
-import { UsersRepository } from "@/repositories/users-repository"
-import { hash } from "bcryptjs"
-import { UserAlreadyExistsError } from "./erros/user-alredy-exists-error"
-import { User } from "@prisma/client"
+import { UsersRepository } from "@/repositories/users-repository";
+import { UserAlreadyExistsError } from "./erros/user-alredy-exists-error";
+import { User } from "@prisma/client";
+import bcrypt from "bcryptjs"; 
 
 interface RegisterUseCaseRequest {
-    name: string
-    email: string
-    password: string
+    name: string;
+    email: string;
+    password: string;
 }
 
 interface RegisterUseCaseResponse {
-    user: User,
+    user: User;
 }
 
 export class RegisterUseCase {
-    constructor(private usersRepository: UsersRepository){
+    constructor(private usersRepository: UsersRepository) {}
 
-    }
-    async execute ({name, email, password}:RegisterUseCaseRequest): Promise<RegisterUseCaseResponse> {
-    
-        const password_hash = await hash(password, 6)
-    
-        const userWithSameEmail = await this.usersRepository.findByEmail(email)
-    
-        if (userWithSameEmail){
-            throw new UserAlreadyExistsError()
+    async execute({
+        name,
+        email,
+        password,
+    }: RegisterUseCaseRequest): Promise<RegisterUseCaseResponse> {
+        const password_hash = await bcrypt.hash(password, 6); 
+
+        const userWithSameEmail = await this.usersRepository.findByEmail(email);
+
+        if (userWithSameEmail) {
+            throw new UserAlreadyExistsError();
         }
-    
+
         const user = await this.usersRepository.create({
             name,
             email,
             password_hash,
-        })
+        });
         return {
             user,
-        }
+        };
     }
 }
